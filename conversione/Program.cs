6fcefce90b95abb79f5c410b714c3d21931320ca
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-05 11:16:19 +0200

    conversioni numeri

--- conversione/Program.cs
+++ conversione/Program.cs
@@ -8,7 +8,7 @@ namespace conversione
         {
             Console.WriteLine("inserisci il numero da convertire");
             int n=int.Parse(Console.ReadLine());
-            Convert.
+            
 
         }
     }
