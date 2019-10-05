4e98ba32a56ac77e27960424bc2877163bd5b421
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-05 11:08:57 +0200

    Add project files.

---
+++ conversione/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace conversione
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci il numero da convertire");
+            int n=int.Parse(Console.ReadLine());
+
+        }
+    }
+}
